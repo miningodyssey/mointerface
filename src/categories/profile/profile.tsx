27faac8dbea27e82a259234f6684a9fa8536c1af
dunnogg <gamemode61@gmail.com>
2024-11-22 17:20:39 +0300

    Change wallet address type

--- src/categories/profile/profile.tsx
+++ src/categories/profile/profile.tsx
@@ -238,7 +238,7 @@ const ProfileCategory: React.FC<ProfileCategoryProps> = ({fadeIn, userData, setU
 
             console.log("Getting user's USDT address...");
             const walletAddressObj = Address.parse(walletAddress);
-            const usersUsdtAddress = await jettonMaster.getWalletAddress(walletAddressObj);
+            const usersUsdtAddress = await jettonMaster.getWalletAddress(walletAddress);
             console.log("User USDT Address:", usersUsdtAddress.toString());
 
             const jettonWallet = tonClient.open(
